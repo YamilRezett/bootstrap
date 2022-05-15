bda326e2bbe61d95f080c04bc4a63ec10f0dc6f8
Yamil Rezett <yamilrezett@gmail.com>
2022-05-15 18:50:28 -0300

    modifique los formulario y el php

--- php/form.php
+++ php/form.php
@@ -2,7 +2,8 @@
     //para capturar la info que se ingresa en los inputs del formulario tengo que crear variables.
 
     $email = $_POST["email"];
-    $celular =$_POST["celular"];
+    $celular = $_POST["celular"];
+    $consulta = $_POST["consulta"] 
     
     //a quien le mando la info
     $para = "yamilrezett@gmail.com";
@@ -10,7 +11,7 @@
 
     //funcion(para que esto funcones) es la funcion que envia la info del from--
 
-    mail($para, $asunto, utf8_decode($email, $celular));
+    mail($para, $asunto, utf8_decode($email, $celular, $consulta));
 
     // funcion que redirecciona al usaurio una vez que comple el form--
     header("location:exito.html");
