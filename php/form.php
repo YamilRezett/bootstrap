b53bb0afbd2f5640662049907837df8b2f888b82
Yamil Rezett <yamilrezett@gmail.com>
2022-05-15 17:23:08 -0300

    cree mi php

---
+++ php/form.php
@@ -0,0 +1,25 @@
+<?php
+    //para capturar la info que se ingresa en los inputs del formulario tengo que crear variables.
+
+    $email = $_POST["email"];
+    $celular =$_POST["celular"];
+    
+    //a quien le mando la info
+    $para = "yamilrezett@gmail.com";
+    $asunto = "este mail fue enviado desde la web";
+
+    //funcion(para que esto funcones) es la funcion que envia la info del from--
+
+    mail($para, $asunto, utf8_decode($email, $celular));
+
+    // funcion que redirecciona al usaurio una vez que comple el form--
+    header("location:exito.html");
+
+
+
+
+
+
+
+
+?>
